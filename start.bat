5f587f3200b713ccddc0390c9d033eecb2333000
MetrPikeska <piter.mikeska@gmail.com>
2026-04-11 12:35:02 +0200

    Remove obsolete files and scripts; delete NEXT_STEPS.md, PG_FEATURESERV_LINUX.md, SECURITY_IMPROVEMENTS.md, and various startup scripts (start.sh, stop.sh, start.bat, stop.bat, chrome-debug.bat, nginx.conf, start-tunnels.sh) to streamline project structure and focus on essential components.

--- start.bat
+++
@@ -1,5 +0,0 @@
-start /min node backend/server.js
-cd pg-featureserv
-start /min pg_featureserv.exe serve
-cd ..
-start index.html
--- stop.bat
+++
@@ -1,3 +0,0 @@
-taskkill /IM node.exe /F
-taskkill /IM pg_featureserv.exe /F
-exit
